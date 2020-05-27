e5dafd0b450ea3cb40846b515e03d1bbdce97f52
Jcod <misha1994g@yandex.ru>
2020-05-27 16:19:06 +0300

    new version

--- addtag.sh
+++ addtag.sh
@@ -1,6 +1,6 @@
 sudo git add .
-sudo git commit -m --new version
-git push "https://Jcodex:MishaMisha1!@github.com/Jcodex/devops"
+sudo git commit -m "new version"
+git push origin task5 "https://Jcodex:MishaMisha1!@github.com/Jcodex/devops"
 sudo git checkout "master"
 value=$(<gradle.properties)
 version="${value/version = /v}"
