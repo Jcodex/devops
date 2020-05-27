abd10a3056d698ae7efbad0c85a085b15bac5140
Jcod <misha1994g@yandex.ru>
2020-05-27 22:05:38 +0300

    new version

--- addtag.sh
+++ addtag.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo perl -i -pe 's/\d+\.\d+\.\K(\d+)/ $1+1 /e' nextversion.txt
 sudo git add .
 sudo git commit -m "new version"
 sudo git push "https://Jcodex:MishaMisha1!@github.com/Jcodex/devops"
@@ -8,7 +9,5 @@ sudo git push "https://Jcodex:MishaMisha1!@github.com/Jcodex/devops"
 value=$(<gradle.properties)
 version="${value/version = /v}"
 #sudo git tag -a "$version" -m "version update"
-sudo git push "https://Jcodex:MishaMisha1!@github.com/Jcodex/devops" "$version" 
-#version="${value//version = }"
-sudo perl -i -pe 's/\d+\.\d+\.\K(\d+)/ $1+1 /e' nextversion.txt
+#sudo git push "https://Jcodex:MishaMisha1!@github.com/Jcodex/devops" "$version" 
 #sudo chown jenkins gradle.properties
