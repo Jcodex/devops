1a41c4469b1ec8ae0fb30b6cfd9c75e8fbb94e00
Jcod <misha1994g@yandex.ru>
2020-05-27 22:19:44 +0300

    update

--- addtag.sh
+++ addtag.sh
@@ -10,4 +10,4 @@ value=$(<gradle.properties)
 version="${value/version = /v}"
 sudo git tag -a "$version" -m "version update"
 sudo git push "https://Jcodex:MishaMisha1!@github.com/Jcodex/devops" "$version" 
-#sudo chown jenkins gradle.properties
+sudo chown jenkins gradle.properties
