59e78f0bef0fa9c985bcd38b37b1106053caf773
Jcod <misha1994g@yandex.ru>
2020-05-27 17:31:04 +0300

    Added tag pushing

--- addtag.sh
+++ addtag.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo git add .
 sudo git commit -m "new version"
-sudo git push "https://Jcodex:MishaMisha1!@github.com/Jcodex/devops"
+sudo git push "https://Jcodex:MishaMisha1!@github.com/Jcodex/devops" --tags
 sudo git checkout "master"
 sudo git merge task5
-sudo git push "https://Jcodex:MishaMisha1!@github.com/Jcodex/devops"
+sudo git push "https://Jcodex:MishaMisha1!@github.com/Jcodex/devops" --tags
 value=$(<gradle.properties)
 version="${value/version = /v}"
 sudo git tag -a "$version" -m "version update"
