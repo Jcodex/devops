02396d100830186ba36b2f98a0e614040ba306cf
Jcod <misha1994g@yandex.ru>
2020-05-27 16:17:19 +0300

    new version

---
+++ addtag.sh
@@ -0,0 +1,7 @@
+sudo git add .
+sudo git commit -m --new version
+git push "https://Jcodex:MishaMisha1!@github.com/Jcodex/devops"
+sudo git checkout "master"
+value=$(<gradle.properties)
+version="${value/version = /v}"
+echo "$version"
