1b8bca936abfbdcb6c6398f5880b60b362bed631
Jcod <misha1994g@yandex.ru>
2020-05-27 18:50:59 +0300

    Update

--- addtag.sh
+++ addtag.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 sudo git add .
 sudo git commit -m "new version"
-sudo git push "https://Jcodex:MishaMisha1!@github.com/Jcodex/devops" --tags
+sudo git push "https://Jcodex:MishaMisha1!@github.com/Jcodex/devops"
 sudo git checkout "master"
 sudo git merge task5
-sudo git push "https://Jcodex:MishaMisha1!@github.com/Jcodex/devops" --tags
+sudo git push "https://Jcodex:MishaMisha1!@github.com/Jcodex/devops"
 value=$(<gradle.properties)
 oldversion=$(<oldversion.txt)
 version="${value/version = /v}"
 sudo git tag -a "$version" -m "version update"
+sudo git push "https://Jcodex:MishaMisha1!@github.com/Jcodex/devops" "v$version" 
 version="${value//version = }"
 sudo chown jenkins gradle.properties
 sudo sed -i "s/$oldversion/$version/g" rolebook.yml
