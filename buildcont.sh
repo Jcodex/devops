cc697d93e955e973c78f90ceaab2386bd101500d
Jcod <misha1994g@yandex.ru>
2020-06-03 18:49:16 +0300

    Added docker build script

---
+++ buildcont.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+value=$(<gradle.properties)
+version="${value//version = }"
+sudo docker build --build-arg version="$version" . -t mycat"$version"
+sudo docker tag mycat localhost:5000/mycat"$version"
+sudo docker push localhost:5000/mycat"$version"
+#sudo docker run -it -d -p 8082:8080 localhost:5000/mycat"$version"
