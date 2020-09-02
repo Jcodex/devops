b577f5726c9df8abfc061dcf99e4b1b5aef0f563
Jcodex <misha1994g@yandex.ru>
2020-09-02 11:31:45 +0300

    Update addtag.sh

--- addtag.sh
+++ addtag.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo git add .
 sudo git commit -m "new version"
-sudo git push "https://Jcodex:MishaMisha1!@github.com/Jcodex/devops"
+sudo git push "https://Jcodex:"
 sudo git checkout "master"
 sudo git merge task6 --no-ff --no-edit
 sudo git add .
