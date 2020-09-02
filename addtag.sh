0e2809ed1951393c61fec9d38f1dfffb10826515
Jcodex <misha1994g@yandex.ru>
2020-09-02 11:33:07 +0300

    Update addtag.sh

--- addtag.sh
+++ addtag.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo git add .
 sudo git commit -m "new version"
-sudo git push "https://Jcodex:MishaMisha1!@github.com/Jcodex/devops"
+sudo git push "https://Jcodex:/Jcodex/devops"
 sudo git checkout "master"
 sudo git merge task6 --no-ff --no-edit
 sudo git add .
@@ -11,5 +11,5 @@ sudo git push "https://Jcodex:
 value=$(<gradle.properties)
 version="${value/version = /v}"
 sudo git tag -a "$version" -m "version update"
-sudo git push "https://Jcodex:MishaMisha1!@github.com/Jcodex/devops" "$version" 
+sudo git push "https://Jcodex:" "$version" 
 sudo chown jenkins gradle.properties
